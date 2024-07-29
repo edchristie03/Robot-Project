6c5c45aff3484e504440ff3511c43b486a214853
Ed Christie <edward.christie24@imperial.ac.uk>
2024-07-29 13:01:30 -0600

    Updated robot program to read in coordinates, clip the coordinates to be between 0-9, and print out the robot's coordinates and quadrant.

--- main.py
+++ main.py
@@ -3,4 +3,28 @@ identifier = 1000
 message = f"Hello. My name is {name}. My ID is {identifier}"
 print(message)
 
+grid_size = 10
 
+row_coordinate = int(input('What is its current row coordinate? '))
+column_coordinate = int(input('What is its current column coordinate? '))
+
+if row_coordinate > (grid_size - 1):
+    row_coordinate = (grid_size - 1)
+elif row_coordinate < 0:
+    row_coordinate = 0
+
+if column_coordinate > (grid_size - 1):
+    column_coordinate = (grid_size - 1)
+elif column_coordinate < 0:
+    column_coordinate = 0
+
+if row_coordinate <= 4 and column_coordinate <= 4:
+    location = 'top left'
+elif row_coordinate <=4 and column_coordinate >=5:
+    location = 'top right'
+elif row_coordinate >=5 and column_coordinate <= 4:
+    location = 'bottom left'
+else:
+    location = 'bottom right'
+ 
+print(f"My current location is ({row_coordinate}, {column_coordinate}). I am in the {location} quadrant.")
