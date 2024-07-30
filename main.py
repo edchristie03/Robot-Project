05f31f920d877efc6a0ff89426220fdc77ef6c14
Ed Christie <edward.christie24@imperial.ac.uk>
2024-07-30 17:59:55 -0600

    Improved robot so that it automatically moves forward until it hits a wall

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ grid_size = 10
 # User defiend variable values and introduction message
 
 name = input('What is the name of the robot? ')
-message = f"Hello. My name is {name}. My ID is {identifier}"
-print(message)
+
+
 
 # Random allocation of starting position 
 
@@ -17,12 +17,13 @@ column_coordinate = random.randint(0,(grid_size - 1))
 
 # Random allocation of starting direction. Created variable 'direction_list' as the output of the random sample is a list. 
 
-directions = ['n','s','e','w']
+directions = ['North','South','East','West']
 direction_list = random.sample(directions,1)
 direction = direction_list[0]
 
 # Deduction of starting quadrant. Boundary defined as <= (grid size/2 - 1) given starting coordinate of 0.
 
+'''
 if row_coordinate <= (grid_size/2 - 1) and column_coordinate <= (grid_size/2 - 1):
     location = 'top left'
 elif row_coordinate <= (grid_size/2 - 1) and column_coordinate > (grid_size/2 - 1):
@@ -31,14 +32,17 @@ elif row_coordinate > (grid_size/2 - 1) and column_coordinate <= (grid_size/2 - 
     location = 'bottom left'
 else:
     location = 'bottom right'
+'''
 
-# Statement of starting position
- 
-print(f"My current location is ({row_coordinate}, {column_coordinate}). I am in the {location} quadrant.")
+# The robot talks
+
+print(f"Hello. My name is {name}. My ID is {identifier}")
+print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.") # I am in the {location} quadrant.
 
 # Statement of starting direction given n,s,e,w input.
 # Could create dictionary. Or randomly sample the words not the letters??
 
+'''
 if direction == 'n':
     print('I am facing North')
 elif direction == 's':
@@ -47,23 +51,46 @@ elif direction == 'e':
     print('I am facing East')
 else:
     print('I am facing West')
+'''
 
-# Moving one step forward if grid size allows
+# Moving to edge of grid, one step at a time
 
-print('Moving one step forward') 
 
-if direction == 'n' and row_coordinate != 0:
-    row_coordinate -= 1
-elif direction == 's' and row_coordinate != (grid_size - 1):
-    row_coordinate += 1
-elif direction == 'e' and column_coordinate != (grid_size - 1):
-    column_coordinate += 1
-elif direction == 'w' and column_coordinate != 0:
-    column_coordinate -= 1
+if direction == 'North':
+    while row_coordinate != 0:
+        print('Moving one step forward') 
+        row_coordinate -= 1
+        print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+    if row_coordinate == 0:
+        print('I have a wall in front of me') 
+elif direction == 'South':
+    while row_coordinate != (grid_size - 1):
+        print('Moving one step forward') 
+        row_coordinate += 1
+        print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+    if row_coordinate == (grid_size - 1):
+        print('I have a wall in front of me')   
+elif direction == 'East':
+    while column_coordinate != (grid_size - 1):
+        print('Moving one step forward') 
+        column_coordinate += 1
+        print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+    if column_coordinate == (grid_size - 1):
+        print('I have a wall in front of me')  
+elif direction == 'West':
+    while column_coordinate != 0:
+        print('Moving one step forward') 
+        column_coordinate -= 1
+        print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+    if column_coordinate == 0:
+        print('I have a wall in front of me') 
+
+
+
 
 # Deduction of new quadrant.
 # Repeated code, could build a function..
-
+'''
 if row_coordinate <= (grid_size/2 - 1) and column_coordinate <= (grid_size/2 - 1):
     location = 'top left'
 elif row_coordinate <= (grid_size/2 - 1) and column_coordinate > (grid_size/2 - 1):
@@ -72,8 +99,8 @@ elif row_coordinate > (grid_size/2 - 1) and column_coordinate <= (grid_size/2 - 
     location = 'bottom left'
 else:
     location = 'bottom right'
+'''
  
-print(f"My current location is ({row_coordinate}, {column_coordinate}). I am in the {location} quadrant.")
 
 
 
