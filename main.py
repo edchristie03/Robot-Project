0e66aeb055b73855020259e9bfa391b27f50d09b
Ed Christie <edward.christie24@imperial.ac.uk>
2024-07-31 11:04:31 -0600

    Improved main.py by separating the code blocks for moving to the boundary and turning 90 degrees. Also made robot turn 90 degrees by adding 1 to the index of the list 'directions'

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# Built in variable values
+# Modules
 
 import random
+
+# Built in variable values
+
 identifier = 1000
 grid_size = 10
 
 # User defiend variable values and introduction message
 
 name = input('What is the name of the robot? ')
-
-
 
 # Random allocation of starting position 
 
@@ -17,7 +18,7 @@ column_coordinate = random.randint(0,(grid_size - 1))
 
 # Random allocation of starting direction. Created variable 'direction_list' as the output of the random sample is a list. 
 
-directions = ['North','South','East','West']
+directions = ['North','East','South','West']
 direction_list = random.sample(directions,1)
 direction = direction_list[0]
 
@@ -53,55 +54,53 @@ else:
     print('I am facing West')
 '''
 
-# Moving to edge of grid, one step at a time
-# Turning clockwise when at edge
-# Moving around grid until finding milk at cell (grid_size - 1), (grid_size - 1)
+# While loop to move robot to bottom right corner of the grid
 
 while row_coordinate != (grid_size - 1) or column_coordinate != (grid_size - 1):
+
+# If-elif block to move to the edge of the grid, one step at a time
 
     if direction == 'North':
         while row_coordinate != 0:
             print('Moving one step forward') 
             row_coordinate -= 1
             print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
-        if row_coordinate == 0:
-            print('I have a wall in front of me')
-            print('Turning 90 degrees clockwise')
-            direction = 'East'
-            print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+
     elif direction == 'South':
         while row_coordinate != (grid_size - 1):
             print('Moving one step forward') 
             row_coordinate += 1
             print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
-        if row_coordinate == (grid_size - 1):
-            print('I have a wall in front of me')
-            print('Turning 90 degrees clockwise')
-            direction = 'West'
-            print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+        
     elif direction == 'East':
         while column_coordinate != (grid_size - 1):
             print('Moving one step forward') 
             column_coordinate += 1
             print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
-        if column_coordinate == (grid_size - 1):
-            print('I have a wall in front of me')
-            print('Turning 90 degrees clockwise')
-            direction = 'South'
-            print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+        
     elif direction == 'West':
         while column_coordinate != 0:
             print('Moving one step forward') 
             column_coordinate -= 1
             print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
-        if column_coordinate == 0:
-            print('I have a wall in front of me')
-            print('Turning 90 degrees clockwise')
-            direction = 'North'
-            print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+
+# Check if in position already to break the while loop
+
+    if row_coordinate == (grid_size - 1) and column_coordinate == (grid_size - 1):
+        break
+
+    print('I have a wall in front of me')
+    print('Turning 90 degrees clockwise')
+
+# Work out index of current direction in list 'directions'
+# Add 1 to index to shift 90 degress
+
+    index = directions.index(direction)
+    index = (index + 1) % 4
+    direction = directions[index]
+    print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
 
 print('I am drinking leché, I am happy!')
-
 
 
 
