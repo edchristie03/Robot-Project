a45148bf8fae7a0ed3a29a116862eaa9cd36aca4
Ed Christie <edward.christie24@imperial.ac.uk>
2024-08-01 13:59:36 -0600

    Built functions for setting up robot

--- main.py
+++ main.py
@@ -1,128 +1,76 @@
-# Modules
 
 import random
-
-# Built in variable values
-
-identifier = 1000
-grid_size = 10
-
-# User defiend variable values and introduction message
-
-name = input('What is the name of the robot? ')
-
-# Random allocation of starting position 
-
-row_coordinate = random.randint(0, (grid_size - 1))
-column_coordinate = random.randint(0,(grid_size - 1))
-
-# Random allocation of starting direction. Created variable 'direction_list' as the output of the random sample is a list. 
-
-directions = ['North','East','South','West']
-direction_list = random.sample(directions,1)
-direction = direction_list[0]
-
-# Deduction of starting quadrant. Boundary defined as <= (grid size/2 - 1) given starting coordinate of 0.
-
 '''
-if row_coordinate <= (grid_size/2 - 1) and column_coordinate <= (grid_size/2 - 1):
-    location = 'top left'
-elif row_coordinate <= (grid_size/2 - 1) and column_coordinate > (grid_size/2 - 1):
-    location = 'top right'
-elif row_coordinate > (grid_size/2 - 1) and column_coordinate <= (grid_size/2 - 1):
-    location = 'bottom left'
-else:
-    location = 'bottom right'
+1. Set up robot
+    a. Get name
+    b. Get identifier
+    c. Get random starting position
+    d. Get random starting direction
+2. Print robot greeting
+    a. Print name and identifier
+    b. Print location and direction
+3. Navigate to Ribena
+    a. While not at (grid_size - 1, grid_size - 1).
+        a. Move in direction to edge
+            a. Move one step.
+            b. Print location and direction after each step.
+            b. At edge, print 'at wall'
+        d. Turn 90 degrees clockwise.
 '''
 
-# The robot talks
+# Set up robot
 
-print(f"Hello. My name is {name}. My ID is {identifier}")
-print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.") # I am in the {location} quadrant.
+def set_up_robot(grid_size):
+    name = input('What is the name of the robot? ')
+    identifier = 1000
+    row, column = get_random_start(grid_size)
+    direction = get_random_direction()
+    return name, identifier, row, column, direction
 
-# Statement of starting direction given n,s,e,w input.
-# Could create dictionary. Or randomly sample the words not the letters??
+def get_random_start(grid_size):
+    """ Random allocation of starting position.
 
-'''
-if direction == 'n':
-    print('I am facing North')
-elif direction == 's':
-    print('I am facing South')
-elif direction == 'e':
-    print('I am facing East')
-else:
-    print('I am facing West')
-'''
+    Args:
+        grid_size (int): The size of the grid.
 
-# While loop to move robot to bottom right corner of the grid
+    Returns:
+        row (int): Row coordinate
+        column (int): Column coordinate
+    """
+    row = random.randint(0, (grid_size - 1))
+    column = random.randint(0,(grid_size - 1))
+    return row, column
 
-while row_coordinate != (grid_size - 1) or column_coordinate != (grid_size - 1):
+def get_random_direction():
+    """ Random allocation of starting direction.
 
-# If-elif block to move to the edge of the grid, one step at a time
+    Args:
+        none
 
-    if direction == 'North':
-        while row_coordinate != 0:
-            print('Moving one step forward') 
-            row_coordinate -= 1
-            print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+    Returns:
+        direction (str): Direction string
+    """
+    directions = ['North','East','South','West']
+    direction_list = random.sample(directions,1)
+    direction = direction_list[0]
+    return direction
 
-    elif direction == 'South':
-        while row_coordinate != (grid_size - 1):
-            print('Moving one step forward') 
-            row_coordinate += 1
-            print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
+def run_simulation(grid_size=10, target_row=9, target_col=9):
+    """ Start robot navigation simulation.
+
+    Args:
+        grid_size (int): The size of the grid. Defaults to 10.
+        target_row (int): The target row coordinate. Defaults to 9.
+        target_col (int): The target column coordinate. Defaults to 9.
+
+    Return:
+    
+    """
+    name, identifier, row, column, direction = set_up_robot(grid_size)
+    print(name, identifier, row, column, direction)
+    
+    pass
+
         
-    elif direction == 'East':
-        while column_coordinate != (grid_size - 1):
-            print('Moving one step forward') 
-            column_coordinate += 1
-            print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
-        
-    elif direction == 'West':
-        while column_coordinate != 0:
-            print('Moving one step forward') 
-            column_coordinate -= 1
-            print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
-
-# Check if in position already to break the while loop
-
-    if row_coordinate == (grid_size - 1) and column_coordinate == (grid_size - 1):
-        break
-
-    print('I have a wall in front of me')
-    print('Turning 90 degrees clockwise')
-
-# Work out index of current direction in list 'directions'
-# Add 1 to index to shift 90 degress
-
-    index = directions.index(direction)
-    index = (index + 1) % 4
-    direction = directions[index]
-    print(f"My current location is ({row_coordinate}, {column_coordinate}). I am facing {direction}.")
-
-print('I am drinking leché, I am happy!')
-
-
-
-# Deduction of new quadrant.
-# Repeated code, could build a function..
-'''
-if row_coordinate <= (grid_size/2 - 1) and column_coordinate <= (grid_size/2 - 1):
-    location = 'top left'
-elif row_coordinate <= (grid_size/2 - 1) and column_coordinate > (grid_size/2 - 1):
-    location = 'top right'
-elif row_coordinate > (grid_size/2 - 1) and column_coordinate <= (grid_size/2 - 1):
-    location = 'bottom left'
-else:
-    location = 'bottom right'
-'''
- 
-
-
-
-
-
-
-
-
-
+grid_size = 10         
+run_simulation(grid_size=grid_size, target_row=(grid_size - 1))
